05285c2fa0aaf8f084e57fa215706e696a0f652e
Martin Saavedra <45432462+martinmsaavedra@users.noreply.github.com>
2024-02-25 19:24:12 -0300

    Update web_scrapping.py

--- chat-flask/web_scrapper/web_scrapping.py
+++ chat-flask/web_scrapper/web_scrapping.py
@@ -21,7 +21,7 @@ def extraer_datos_servicios():
     scroll_infinito("https://promptior.ai/#services", espera=2, scrolls=5)
     servicios = driver.find_elements(By.CLASS_NAME, 'service-text')
     servicios_list = [servicio.text for servicio in servicios]
-    with open('chat-flask/data/services_data.json', 'w', encoding='utf-8') as f:
+    with open('data/services_data.json', 'w', encoding='utf-8') as f:
         json.dump(servicios_list, f, ensure_ascii=False, indent=4)
     
     # for servicio in servicios:
@@ -32,7 +32,7 @@ def extraer_datos_sobre_nosotros():
     scroll_infinito("https://promptior.ai/about", espera=2, scrolls=5)
     texto_sobre_nosotros = driver.find_elements(By.CLASS_NAME, 'about-text')
     sobre_nosotros_list = [texto.text for texto in texto_sobre_nosotros] 
-    with open('chat-flask/data/about_data.json', 'w', encoding='utf-8') as f:
+    with open('data/about_data.json', 'w', encoding='utf-8') as f:
         json.dump(sobre_nosotros_list, f, ensure_ascii=False, indent=4)
         
     # for texto in texto_sobre_nosotros:
